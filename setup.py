ea5893f84baf26e3a12ea5fe43319587a8ca7fc5
Daniel Jalova <djalova@us.ibm.com>
2018-12-12 16:04:59 -0800

    Add authors

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(name='maxfw',
       version='0.1',
-      description='',
-      url='',
+      description='A package to simplify the creation of MAX models',
+      url='https://github.com/IBM/MAX-Framework',
       author='CODAIT',
-      author_email='',
+      author_email='djalova@us.ibm.com, nickp@za.ibm.com, brendan.dwyer@ibm.com',
       license='Apache',
       packages=['maxfw', 'maxfw.core', 'maxfw.model'],
-      zip_safe=True)
+      zip_safe=True,
+      install_requires=[
+        'flask-restplus==0.11.0',
+        'flask-cors',
+        ],
+    )
