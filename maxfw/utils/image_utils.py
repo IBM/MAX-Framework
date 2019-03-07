12354f85614708fabaf7b2bf65f55fc187798788
Karthik Muthuraman <karthik.muthuraman@ibm.com>
2019-03-07 13:58:31 -0800

    added test image and code. modularized preprocessing.

--- maxfw/utils/image_utils.py
+++ maxfw/utils/image_utils.py
@@ -10,53 +10,61 @@ import numpy as np
 class ImagePreprocessor:
     '''A Pillow-based image preprocessing tool adapted for MAX APIs.'''
 
-    def __init__(self, remove_alpha_channel=True, grayscale=False, normalize=False, standardize=False):
+    def __init__(self, remove_alpha_channel=False, grayscale=False, normalize=False, standardize=False, rotate=None, resize=None):
         '''
-        :param grayscale: Convert an RGB image to grayscale. This reduces the number of channels to one.
-        :param normalize: Scale the pixel values to interval [0, 1].
-        :param standardize: Scale the pixel values to interval [-1, 1].
-        :param remove_alpha_channel:
+        :param grayscale:   Boolean - Convert an RGB image to grayscale. This reduces the number of channels to one.
+        :param normalize:   Boolean - Scale the pixel values to interval [0, 1].
+        :param standardize: Boolean -  Scale the pixel values to interval [-1, 1].
+        :param remove_alpha_channel: Boolean - Removes the alpha channel and converts image to RGB.
+        :param rotate: float - Degrees counterclockwise to rotate.
+        :param resize: tuple - The requested size in pixels, as a 2-tuple: (width, height).
         '''
         self.grayscale = grayscale
         self.normalize = normalize
         self.standardize = standardize
         self.remove_alpha_channel = remove_alpha_channel
+        self.rotate = rotate
+        self.resize = resize
+
+
+        # Param sanity check
         assert self.standardize in [True, False]
         assert self.normalize in [True, False]
         assert self.remove_alpha_channel in [True, False]
         assert int(normalize) + int(standardize) < 2, "Setting both normalize and stardize to True is not possible."
         
+        # Configure image mode, mapping user params to PIL modes.
+        self._image_mode = None
+        if self.grayscale:
+            self._image_mode = 'L'
+        elif self.remove_alpha_channel:
+            self._image_mode = 'RGB'
+        
 
     def preprocess_imagedata(self, image_data):
         '''
-        Read the image from a bytestream.
+        Read the image from a bytestream, and apply all necessary transofrmations.
         If the PIL.Image module is unable to process the stream, a Flask error with status code 400 will be raised.
 
         Input: Image bytes.
         Returns: Numpy array.
         '''
+        
         try:
-            im = Image.open(io.BytesIO(image_data))
+            im = Image.open(io.BytesIO(image_data)).convert(self._image_mode)
+            
         except:
             abort(400, "The provided input is not a valid image. Make sure that the bytestream of an image is passed as input.")
 
-        # send the image through the pipeline
-        if self.grayscale:
-            try:
-                assert im.mode=='RGB'
-                im = im.Convert('L')
-            except:
-                abort(400, "The given image is not in a RGB format to be converted to grayscale")
-        im = np.array(im)
+        if self.rotate:
+            im = im.rotate(self.rotate)
         
+        if self.resize:
+            im = im.resize(self.resize)
+        
+        im = np.array(im)
         if self.normalize:
             im = im / np.linalg.norm(im)
-        
-        if self.remove_alpha_channel:
-            assert im.shape[-1] == 4
-            im = im[:,:,1:4]
-
-
         # return numpy array
         return im
 
