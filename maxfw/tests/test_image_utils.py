29426fa939fab4ef5c5b7ebc4df447a81d798fb6
Karthik Muthuraman <karthik.muthuraman@ibm.com>
2019-03-25 11:10:28 -0700

    added individual test cases.

--- maxfw/tests/test_image_utils.py
+++ maxfw/tests/test_image_utils.py
@@ -14,20 +14,47 @@ stream = io.BytesIO()
 Image.open('maxfw/tests/test_image.jpg').convert('RGBA').save(stream, 'PNG')
 test_input = stream.getvalue()
 
-def test_imagepreprocessor():
+def test_imagepreprocessor_read():
     '''
     Test the ImagePreprocessor.
     '''
-
     # Test reshape
-    preprocessor = ImagePreprocessor(rotate_angle=180,resize_shape=(100,100), verbose=True, normalize=False)
+    preprocessor = ImagePreprocessor(verbose=True)
+    assert preprocessor.preprocess_imagedata(test_input).shape == (678, 1024, 3)
+
+
+def test_imagepreprocessor_rotate():
+    '''
+    Test the ImagePreprocessor image read.
+    '''
+    # Test reshape
+    preprocessor = ImagePreprocessor(rotate_angle=180, verbose=True)
+    preprocessor_no_rotate = ImagePreprocessor(rotate_angle=0, verbose=True)
+    x = preprocessor.preprocess_imagedata(test_input)
+    y = preprocessor_no_rotate.preprocess_imagedata(test_input)
+    assert x[-1,:,:].all() == y[0,:,:].all()
+
+def test_imagepreprocessor_resize():
+    '''
+    Test the ImagePreprocessor resize function.
+    '''
+    # Test reshape
+    preprocessor = ImagePreprocessor(resize_shape=(100,100), verbose=True)
     assert preprocessor.preprocess_imagedata(test_input).shape == (100, 100, 3)
 
-    # Test grayscale
-    preprocessor = ImagePreprocessor(resize_shape=(100, 100), grayscale=True, verbose=True)
-    assert preprocessor.preprocess_imagedata(test_input).shape == (100, 100)
+def test_imagepreprocessor_grayscale():
+    '''
+    Test the ImagePreprocessor grayscale function.
+    '''
+    preprocessor = ImagePreprocessor(grayscale=True, verbose=True)
+    assert preprocessor.preprocess_imagedata(test_input).shape == (678, 1024)
 
+def test_imagepreprocessor_output():
+    '''
+    Test the ImagePreprocessor file output function.
+    '''
     # test file output
+    preprocessor = ImagePreprocessor(verbose=True)
     preprocessor.preprocess_imagedata(test_input, png_file_path='test_output.png')
     # test if the output image is a valid image
     Image.open('test_output.png')
@@ -44,5 +71,12 @@ def test_imagepostprocessor():
 
 if __name__ == '__main__':
     pytest.main([__file__])
-    test_imagepreprocessor()
+    # Preprocessing functions
+    test_imagepreprocessor_read()
+    test_imagepreprocessor_rotate()
+    test_imagepreprocessor_resize()
+    test_imagepreprocessor_grayscale()
+    test_imagepreprocessor_output()
+
+    # Postprocessing
     test_imagepostprocessor()
