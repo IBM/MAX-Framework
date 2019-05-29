1a89e2df6bf495272c5bc3fb60629234b53c0faf
Daniel Jalova <djalova@us.ibm.com>
2019-05-28 17:06:35 -0700

    Remove abstract method decorator from optional methods (#28)
    * Remove abstract decorator from init and pre/post processing
    
    * Bump version
    
    * Remove implementation

--- maxfw/model/model.py
+++ maxfw/model/model.py
@@ -2,17 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class MAXModelWrapper(ABC):
-    @abstractmethod
     def __init__(self, path=None):
         """Implement code to load model here"""
         pass
 
-    @abstractmethod
     def _pre_process(self, x):
         """Implement code to process raw input into format required for model inference here"""
         return x
 
-    @abstractmethod
     def _post_process(self, x):
         """Implement any code to post-process model inference response here"""
         return x
@@ -20,7 +17,7 @@ class MAXModelWrapper(ABC):
     @abstractmethod
     def _predict(self, x):
         """Implement core model inference code here"""
-        return x
+        pass
 
     def predict(self, x):
         pre_x = self._pre_process(x)
