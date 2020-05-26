0912eed10c467339e15435d5f846fed0c94ff5e0
Hong Xu <hong@topbug.net>
2020-05-26 15:54:57 -0700

    Update maxfw/utils/image_functions.py
    Co-authored-by: Brendan Dwyer <brendan.dwyer@ibm.com>

--- maxfw/utils/image_functions.py
+++ maxfw/utils/image_functions.py
@@ -176,7 +176,7 @@ def standardize(img, mean=None, std=None):
             std = np.array([std] * channels).reshape((channels,))
         elif isinstance(std, Sequence):
             # convert a sequence to the right dimensions
-            if np.sum([not isinstance(x, (int, float)) for x in std]) != 0:
+            if np.issubdtype(mean.dtype, np.number):
                 raise ValueError('The sequence `std` can only contain numbers.')
             if len(std) != channels:
                 raise ValueError('The size of the `std` array must correspond to the number of channels in the image.')
